2469757616207cb7f1e244877badc7d0dad7246f
finalstage69 <lordshredder@yahoo.de>
2020-04-18 17:43:55 +0200

    Initial Commit

---
+++ Ueb01/cmake-build-debug/CMakeFiles/Ueb01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ueb01.dir/main.cpp.obj"
+  "Ueb01.exe"
+  "Ueb01.exe.manifest"
+  "Ueb01.lib"
+  "Ueb01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ueb01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
