182beec68010200626672f7d23b01c34685a843b
schorrnico <schorrnico@freenet.de>
2020-04-19 17:55:14 +0200

    for schleife

--- Ueb01/main.cpp
+++ Ueb01/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
+void forschleife();
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     for (int i = 0; i <4; ++i) {
         //changing stuff here
     }
 
-    for (int j = 0; j < 8; ++j) {
-        
-    }
+    forschleife();
     return 0;
 }
+
+void forschleife() {
+    for (int j = 0; j < 8; ++j) {
+
+    }
+}
+
+int
