0823e2041351a34413465e5d61be687d61449461
finalstage69 <lordshredder@yahoo.de>
2020-04-22 16:12:50 +0200

    Merge

---
+++ Ueb01/PersonStorage.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Pai on 22/04/2020.
+//
+
+#include "PersonStorage.h"
+
+void PersonStorage::lesePerson(Person *person) {
+
+}
+
+void PersonStorage::sortierePersonen(Person *) {
+
+}
