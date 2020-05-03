e6b8d12587646a080c7cfd9d4c22f8866fa4cb38
schorrnico <schorrnico@freenet.de>
2020-05-03 23:38:17 +0200

    added class "Article"

---
+++ Ueb02/Article.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#pragma once
+
+
+class Article {
+    public:
+        Article(int articleNr, string description, int stock = 0);
+        void addQuantity(int amount);
+        void removeQuantity(int amount);
+        int getArticleNr();
+        int getStock();
+        string getDescription();
+        void setDescription(string newDescription);
+    private:
+        int articleNr;
+        int stock;
+        string description;
+};
+
+
