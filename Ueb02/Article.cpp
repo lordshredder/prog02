d8e5dec7d80b24b84927f780dfea8a19d05872db
finalstage69 <lordshredder@yahoo.de>
2020-05-06 10:44:02 +0200

    Fixes + added definitions

--- Ueb02/Article.cpp
+++ Ueb02/Article.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 #include "Article.h"
+
+Article::Article(int articleNr, string description, int stock) {
+
+}
+
+void Article::addQuantity(int amount) {
+
+}
+
+void Article::removeQuantity(int amount) {
+
+}
+
+int Article::getArticleNr() {
+    return 0;
+}
+
+int Article::getStock() {
+    return 0;
+}
+
+string Article::getDescription() {
+    return std::__cxx11::string();
+}
+
+void Article::setDescription(string newDescription) {
+
+}
--- Ueb02/Article.h
+++ Ueb02/Article.h
@@ -1,6 +1,6 @@
+#pragma once
 #include <iostream>
 using namespace std;
-#pragma once
 
 
 class Article {
