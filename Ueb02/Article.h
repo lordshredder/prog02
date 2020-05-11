f883b0118b97ba864ea2cd10aedcffe0cc898cb0
schorrnico <schorrnico@freenet.de>
2020-05-11 15:37:43 +0200

    added doxygen comments

--- Ueb02/Article.h
+++ Ueb02/Article.h
@@ -1,7 +1,22 @@
+/**
+ *
+ *  @file Article.h
+ *  @authors David Berres, Nico Schorr
+ *  @date 11.05.2020
+ */
+
+
 #pragma once
 #include <iostream>
 #include <string>
 using namespace std;
+
+/**
+ *  A class called Article, used to simulate inventory management by
+ *  working with an article number, a description and a given stock.
+ *  The class is also able to add and remove quantity from the stock as well as
+ *  changing the description of the article.
+ */
 
 
 
