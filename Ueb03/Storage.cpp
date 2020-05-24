b7c1702dc998f9c8cf676983ec0098ec29bd050a
schorrnico <schorrnico@freenet.de>
2020-05-24 16:09:53 +0200

    added Storage.ccp and .h

---
+++ Ueb03/Storage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nico on 24.05.2020.
+//
+
+#include "Storage.h"
---
+++ Ueb03/Storage.h
@@ -0,0 +1,8 @@
+//
+// Created by Nico on 24.05.2020.
+//
+
+#ifndef UEB02_STORAGE_H
+#define UEB02_STORAGE_H
+
+#endif //UEB02_STORAGE_H
